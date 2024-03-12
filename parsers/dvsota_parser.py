635589a0005463c9eceabf5be0f2f96a553c2929
shhhhhnap <153055602+shhhhhnap@users.noreply.github.com>
2024-03-12 09:54:44 +0300

    Update dvsota_parser.py

--- parsers/dvsota_parser.py
+++ parsers/dvsota_parser.py
@@ -8,7 +8,7 @@ def dvsota(url: str):
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
         'accept': '*/*'
     }
-    req = requests.get(url)
+    req = requests.get(url, headers=headers)
     if req.status_code == 200:
         soup = BeautifulSoup(req.text, 'lxml')
         product_name = soup.find('h1', class_='product_name title').text.strip().rstrip('\n')
